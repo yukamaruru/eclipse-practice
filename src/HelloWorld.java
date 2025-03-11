2f31dedcc92794bd1ec150e1fccd2ddf9277e986
yukamaru <yuun.vv07@gmail.com>
2025-03-11 21:21:32 +0900

    初コミット

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//Hello World!の出力
+		System.out.println("Hello World!");
+		System.out.println("こんにちは！！");
+		System.out.println(123);
+		System.out.println("おはよう");
+		System.out.println(0);
+		System.out.println("例外が発生しました。");
+		System.out.println("例外が");
+		System.out.println("発生しました。");
+		System.out.println("例外が\n発生しました。");
+		System.out.println("例外が発生しました。");
+		
+	}
+
+}
