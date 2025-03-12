56e6037e5ec4b97cfe02969a02701558ea5cc0da
yukamaru <yuun.vv07@gmail.com>
2025-03-12 21:13:30 +0900

    HelloWorld.javaを修正し、variable.javaを追加

---
+++ src/variable.java
@@ -0,0 +1,34 @@
+
+public class variable {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String str1 = "鈴木";
+		System.out.println(str1);
+		
+		String str2 = "Tanaka";
+		System.out.println(str2);
+		
+		String tel = "090-1234-5678";
+		System.out.println(tel);
+		
+		char char1 = 'A';
+		System.out.println(char1);
+		
+		int num1 = 12345;
+		System.out.println(num1);
+		
+		boolean boo = true;
+		System.out.println(boo);
+		
+		int a = 3+3;
+		System.out.println(a);
+		
+		String b = "3+3";
+		System.out.println(b);
+		
+		String abc = "山田";
+		System.out.println(abc + "さん");
+	}
+
+}
