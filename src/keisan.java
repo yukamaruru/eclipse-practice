21f4d0128708ce8653e156dce55f4813338e4c95
yukamaru <yuun.vv07@gmail.com>
2025-03-13 20:47:40 +0900

    keisan.javを追加

---
+++ src/keisan.java
@@ -0,0 +1,88 @@
+
+public class keisan {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a1 = 4 + 3;
+		System.out.println(a1);
+		
+		int a2 = -8 -3;
+		System.out.println(a2);
+		
+		var a3 = -5 * 3;
+		System.out.println(a3);
+		
+		var a4 =4 / 2;
+		System.out.println(a4);
+		
+		int a = 0 / 4;
+		System.out.println(a);
+		
+		int a5 = 0 / -3;
+		System.out.println(a5);
+		
+		int a6 = 10 / 3;
+		System.out.println(a6);
+		
+		int a7 = -10 / 3;
+		System.out.println(a7);
+		
+		int aa = 5 / 2;
+		System.out.println(aa);
+		
+		int bb = -23 / 2;
+		System.out.println(bb);
+		
+		int b1 = 10 / 3;
+		System.out.println(b1);
+		
+		float b2 = (float)10 / 3;
+		System.out.println(b2);
+		
+		float b3 = 5 / 2;
+		System.out.println(b3);
+		
+		float b4 = (float)5 / 2;
+		System.out.println(b4);
+		
+		float b5 = (float)10 / 3;
+		System.out.println(b5);
+		
+		float b6 = (float)10 / 3;
+		System.out.println(b6);
+		
+		double b7 = (double)10 / 3;
+		System.out.println(b7);
+		
+		int c = 7 % 3;
+		System.out.println(c);
+		
+		System.out.println(5 % 3);
+		
+		int c1 = 6 % 3;
+		System.out.println(c1);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
