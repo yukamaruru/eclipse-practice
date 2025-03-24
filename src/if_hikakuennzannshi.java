65496517abad81d8f70c30f67515095f952477c6
yukamaru <yuun.vv07@gmail.com>
2025-03-24 21:54:50 +0900

    新しく加算子・減算子とif文のクラスを追加

---
+++ src/if_hikakuennzannshi.java
@@ -0,0 +1,118 @@
+
+public class if_hikakuennzannshi {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+			int a = 10;
+			if(a < 20) {
+				System.out.println( "A" );
+			
+			
+			int b = 20;
+			if(b <= 30) {
+				System.out.println( "B" );
+			
+			String c = "山田";
+			if(c.equals( "山田" )) {
+				System.out.println(c+"さん");
+			}
+			
+			int d = 5;
+			if(d >= 5 || d <= 0) {
+				System.out.println( "B" );
+			}
+			
+			int e = 7;
+			if(e > 8) {
+				System.out.println( "A" );
+			}else if(e < 10) {
+				System.out.println( "B" );
+			}
+			
+			int a1 = 11;
+			if(a1 > 8) {
+				System.out.println( "A" );
+			}else if(a1 < 10) {
+				System.out.println( "B" );
+			}
+			
+			int b1 = 3;
+			if(b1 > 5) {
+				System.out.println("A");
+			}else {
+				System.out.println("Z");
+			}
+			
+			int c1 = 20;
+			if(c1 < 8) {
+				System.out.println("A");
+			}else if(c1 == 8) {
+				System.out.println("B");
+			}else {
+				System.out.println("C");
+			}
+			
+			int d1 = 18;
+			if(d1 >= 20) {
+				System.out.println("成人");
+			}else {
+				System.out.println("未成年");
+			}
+			
+			int age = 35;
+			if(age < 20) {
+				System.out.println("未成年");
+			}else if(age > 80) {
+				System.out.println("高齢者");
+			}else {
+				System.out.println("成人");
+			}
+			
+			int age1 = 35;
+			if(age1 < 20) {
+				System.out.println("未成年");
+			}else if(age1 >= 20 && age1 <= 80) {
+				System.out.println("成人");
+			}else {
+				System.out.println("高齢者");
+			}
+			
+			int aa = 5;
+			if(aa % 2 == 0) {
+				System.out.println("偶数");
+			}else {
+				System.out.println("奇数");
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		}
+	}
+
+}
---
+++ src/kagensanshi.java
@@ -0,0 +1,63 @@
+
+public class kagensanshi {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 10;
+		System.out.println(++a);
+		
+		int b = 5;
+		System.out.println(++b);
+		
+		int c = 10;
+		System.out.println(++c);
+		System.out.println(++c);
+		System.out.println(++c);
+		System.out.println(++c);
+		
+		int d = 10;
+		System.out.println(d++);
+		
+		int a1 = 5;
+		System.out.println(a1++);
+		
+		int b1 = 10;
+		System.out.println(b1++);
+		System.out.println(b1++);
+		System.out.println(b1++);
+		System.out.println(b1++);
+		
+		int e = 10;
+		System.out.println(--e);
+		
+		int f = 10;
+		System.out.println(--f);
+		System.out.println(--f);
+		System.out.println(--f);
+		System.out.println(--f);
+		
+		int g = 10;
+		System.out.println(g--);
+		
+		int h = 10;
+		System.out.println(h--);
+		System.out.println(h--);
+		System.out.println(h--);
+		System.out.println(h--);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
