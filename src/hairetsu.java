23f51855ddd8b69cb744d6d31244d042cf5fa536
yukamaru <yuun.vv07@gmail.com>
2025-03-27 21:28:37 +0900

    新しくhairetsu.javaとtajigenhairetsu.javaを追加

---
+++ src/hairetsu.java
@@ -0,0 +1,35 @@
+
+public class hairetsu {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String[] name = new String[3];
+		name[0]="田中";
+		name[1]="高橋";
+		name[2]="斉藤";
+			System.out.println(name[0]);
+		
+		String[] name1 = {"田中","高橋","斉藤"};
+			System.out.println(name1[0]);
+			
+		char[] c = {'x','y','z'};
+			System.out.println(c[0]);
+				
+		char[] d = {'x','y','z'};
+			 System.out.println(c[1]);
+			 
+		char[] e = {'x','y','z'};
+			System.out.println(c[2]);
+			
+		int[] i = {1,5,10};
+			System.out.println(i[0]);
+			
+		int[] j = {1,5,10};
+			System.out.println(j[1]);
+			
+		String[] name2 = {"田中","高橋","斉藤"};
+			name2[0]="加藤";
+				System.out.println(name2[0]);
+	}
+
+}
---
+++ src/tajigenhairetsu.java
@@ -0,0 +1,72 @@
+
+public class tajigenhairetsu {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String[][] country =
+			{
+					{"日本","タイ"},
+					{"アメリカ","ブラジル"},
+					{"フランス","ロシア"}
+			};
+			System.out.println(country[0][0]);
+			
+		String[][] country1 =
+			{
+					{"日本","タイ"},
+					{"アメリカ","ブラジル"},
+					{"フランス","ロシア"}
+			};
+		System.out.println(country1[0][1]);
+		
+		String[][] country2 =
+			{
+					{"日本","タイ"},
+					{"アメリカ","ブラジル"},
+					{"フランス","ロシア"}
+			};
+		System.out.println(country2[1][0]);
+		
+		String[][] country3 =
+			{
+					{"日本","タイ"},
+					{"アメリカ","ブラジル"},
+					{"フランス","ロシア"}
+			};
+		System.out.println(country3[2][1]);
+		
+		String[][] country4 = new String[3][2];
+		country4[0][0]="日本";
+		country4[0][1]="タイ";
+		country4[1][0]="アメリカ";
+		country4[1][1]="ブラジル";
+		country4[2][0]="フランス";
+		country4[2][1]="ロシア";
+		System.out.println(country[0][0]);
+		
+		
+		String[][] country5 = new String[3][2];
+		country5[0][0]="日本";
+		country5[0][1]="タイ";
+		country5[1][0]="アメリカ";
+		country5[1][1]="ブラジル";
+		country5[2][0]="フランス";
+		country5[2][1]="ロシア";
+		System.out.println(country[1][1]);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
