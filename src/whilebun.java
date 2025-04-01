ba25cb86721ed0ef705f1c578f8e37d939111141
yukamaru <yuun.vv07@gmail.com>
2025-04-01 23:43:17 +0900

    新しくwhilebun.javaを追加

---
+++ src/whilebun.java
@@ -0,0 +1,98 @@
+
+public class whilebun {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i = 1;
+		while(i <= 5) {
+			System.out.println(i);
+			i++;
+		}
+		
+		int a = 0;
+		while(a < 10) {
+			System.out.println(a);
+			a++;
+		}
+		
+		int b = 5;
+		while(b < 10) {
+			System.out.println(b);
+			b++;
+		}
+		
+		int c = 5;
+		while(c > 0) {
+			System.out.println(c);
+			c--;
+		}
+		
+		int d = 10;
+		while(d >= 5) {
+			System.out.println(d);
+			d--;
+		}
+		
+		int e = 1;
+		while( e <= 10) {
+			System.out.println(e);
+			e+=2;
+		}
+		
+		int f = 1;
+		while( f <= 10) {
+			System.out.println(f);
+			f+=3;
+		}
+		
+		int g = 10;
+		while(g >= 1) {
+			System.out.println(g);
+			g-=2;
+		}
+		
+		int h = 10;
+		while( h >= 1) {
+			System.out.println(h);
+			h-=3;
+		}
+		
+		int j = 1;
+		while( j <= 5) {
+			System.out.println("★");
+			j++;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
